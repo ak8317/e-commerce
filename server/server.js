466b0cc6fc361561bf9ae13ad585db5957060184
ak8317 <ankitkumar8317@gmail.com>
2021-02-09 20:49:49 +0530

    added server basic setup with usermodel

---
+++ server/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
+const fs = require('fs');
+require('dotenv').config();
+
+//import routes
+//const authRoutes = require('./routes/auth');
+//app
+const app = express();
+
+//db
+mongoose
+  .connect(process.env.DATABASE, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`DB connected`))
+  .catch((err) => console.log(`DB connection error ${err}`));
+
+//middleware
+app.use(morgan('dev'));
+app.use(express.json({ extended: false }));
+app.use(cors());
+
+//route
+//app.use('/api', authRoutes);
+fs.readdirSync('./routes').map((r) => {
+  app.use('/api', require('./routes/' + r));
+});
+
+//port
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
